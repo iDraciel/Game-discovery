978e0dcee52fb957e51fdd413c6afce81394acdb
Purushottam <iDraciel@users.noreply.github.com>
2020-05-01 14:07:07 +0530

    added top ordered games to home page

--- src/HomeComp.js
+++ src/HomeComp.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Home from './components/Home/Home.js';
-
+import TopOrdered from './components/TopOrdered.js';
 const HomeComp =()=>{
     return(
         <div>
       <Home/>
 
-    
+    <TopOrdered/>
     
       </div>
     );
---
+++ src/components/TopOrdered.js
@@ -0,0 +1,63 @@
+import React,{Component} from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import './Games.css';
+import axios from 'axios';
+
+class TopOrdered extends Component {
+    state = {
+        users: [],
+      
+      };
+    
+      getUsers() {
+        axios
+          .get(`https://cors-anywhere.herokuapp.com/https://api.rawg.io/api/games?dates=2019-01-01,2020-12-31&ordering=-added`)
+          .then(response =>
+            response.data.results.map(user => ({
+              name: `${user.name}`,
+              image: `${user.background_image}`
+            }))
+          )
+          .then(users => {
+            this.setState({
+              users:users.slice(0,8),
+          
+            });
+          })
+      }
+    
+      componentDidMount() {
+        this.getUsers();
+      }
+    
+      render() {
+        const { users } = this.state;
+        return (
+          <React.Fragment>
+            <h1>Top Ordered Games</h1>
+            
+            <div className="live-update">
+        <div className="inner">
+        </div>
+        <h6 className="header-6">Live Updates</h6>
+        </div>
+            <div className="row">
+           { users.map(user => {
+                  const { name,image } = user;
+                  return (
+                         <div className="column" key={name}>
+                      <div className="card">
+                        <img src={image} className="image" style={{width:"300",height:"300"}} alt={name} />
+                        <p>{name}</p>
+                      <hr />
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </React.Fragment>
+        );
+      }
+    
+}
+export default TopOrdered;
