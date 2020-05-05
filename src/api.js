a336b78dfa19c4bb6cbc2da1c1d4691189e5f39a
Purushottam <iDraciel@users.noreply.github.com>
2020-05-05 21:05:52 +0530

    changed id

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 let api = axios.create({
  headers:  {
-
+    'Client-ID':'yusgahii8qpbeaab0yi3dyh3wg6yw9',
      'accept': 'application/vnd.twitchtv.v5+json',
      'Authorization':'Bearer usj7focn70capa7oeu03f09lh3295s'
      
