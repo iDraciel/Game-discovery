a7b58e40c26dfdd4ced620ccb58909b434820b12
Mohammed Abraz <callmeabraz@gmail.com>
2020-05-11 18:35:15 +0530

    New Footer Added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,21 +1,60 @@
 import React from 'react';
 import './Footer.css';
-import Play from './Play.png';
+import Logo from '../Toolbar/Logo.png';
+
+import {NavLink} from 'react-router-dom';
+
 const Footer =() =>{
     return(<div>
-         <footer className="footer">
-  <div className="footer-container">
-    <img src={Play} alt="footer-pic" className="footer-pic"/>
-    <div className="footer-para">
-    <p className="first-para" >GameSpaceX.com is a website about PC and Console games that shows updates, streams, and news about games. Check out the latest update of upcoming AAA games and live streams.
-  Legal notice: GameSpaceX.com does NOT host any pirated or illegal content, does NOT provide links to any pirated or illegal sites.
-    GameSpaceX uses Pictures and Videos from Twitch and Rawg.io. Copyrights are owned by those providers and can be removed upon official request.GameSpaceX is solemnly an informational website that provides updates on mostly PC games and view watch of games.
-    GameSpaceX encourages users to purchase games from market and at least one purchase link on each game page is provided.</p>
-  </div>
-    </div>
-    <div className="footer-social">
-      <p className="footer-rights">copyright © 2020 GameSpaceX</p>
-      </div>
+         
+      <footer className="footer">
+       
+          <div className="footer-logo-container">
+            <img src={Logo} alt="footer-pic" className="footer-pic"/>
+            <h2 className= "foogter-logo-name">GameSpaceX</h2>
+          </div>
+
+          <div className="footer-info">
+            Explore a collection of video games and Esports
+          </div>
+
+          <div className="footer-navigation-items">
+              <ul>
+
+                <li><NavLink to="/about-us" >About Us</NavLink></li>
+                <li><a href="/">Privacy Policy</a></li>
+                <li><a href="/">Terms and Conditions</a></li>
+                   
+              </ul>
+          </div>
+
+          {/* <div className= "footer-legal">
+            <p>
+            GameSpaceX.com is a website about PC and Console games that shows updates, streams, and news about games.<b></b>
+            <b>Check out the latest update of upcoming AAA games and live streams.</b>
+            <b>Legal notice: GameSpaceX.com does NOT host any pirated or illegal content, does NOT provide links to any pirated or illegal sites.</b>
+            <b>GameSpaceX uses Pictures and Videos from Twitch and Rawg.io. Copyrights are owned by those providers and can be removed upon official request.</b>
+            <b>GameSpaceX is solemnly an informational website that provides updates on mostly PC games and view watch of games.</b>
+            <b>GameSpaceX encourages users to purchase games from market and at least one purchase link on each game page is provided.</b>
+            </p>
+          </div> */}
+
+          <div className = "footer-rights">
+
+            <div className= "footer-notice">
+              Video games, pictures, all trademarks, and registered trademarks are the property of their respective owners.
+            </div>
+
+            <div className="spacer" />
+
+            <div className= "footer-notice">
+              © 2020 GameSpaceX
+            </div>
+          </div>
+
+        
+
+          
   </footer>
   </div>
 
