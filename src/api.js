edf318b5e055fc618ef9a87a561aec6f500d4ca7
Purushottam <iDraciel@users.noreply.github.com>
2020-11-30 18:49:00 +0530

    added new token

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ let api = axios.create({
  headers:  {
    'Client-ID':'yusgahii8qpbeaab0yi3dyh3wg6yw9',
      'accept': 'application/vnd.twitchtv.v5+json',
-     'Authorization':'Bearer krx5ah3fwm0fmm0k1kbex97k3dlblm'
+     'Authorization':'Bearer p93gkuxz11zgnmrg5px1k73mnugqti'
      
  },
    
